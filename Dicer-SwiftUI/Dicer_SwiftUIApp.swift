0bdb1dbfddea398648c76ae0f25f17fe59a7536e
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-16 12:52:36 +0300

    Initial Commit

---
+++ Dicer-SwiftUI/Dicer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicer_SwiftUIApp.swift
+//  Dicer-SwiftUI
+//
+//  Created by ALEKSEY SUSLOV on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dicer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
